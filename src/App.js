0deda1c41669b8edfab41c289628a0326bbd4e41
niss-shana <134854766+niss-shana@users.noreply.github.com>
2024-04-21 21:30:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import CreateUpdates from './pages/CreateUpdates.js';
 import Results from './pages/Results.js';
 import CreateSurveys from './pages/CreateSurveys.js';
 import ForgotPasswordPage from './pages/ForgotPasswordPage.js';
-import OfficialNotification from './pages/OfficialNotification.js';
+import officialNotification from './pages/officialNotification.js';
 import MyAccount from './pages/MyAccount.js';
 import EditProfile from './pages/EditProfile.js';
 
@@ -36,7 +36,7 @@ function App() {
           <Route path="/CreateSurveys" Component={CreateSurveys} />
           <Route path="/Results" Component={Results} />
           <Route path="/ForgotPasswordPage" Component={ForgotPasswordPage} />
-          <Route path="/OfficialNotification" Component={OfficialNotification} />
+          <Route path="/officialNotification" Component={officialNotification} />
           <Route path="/MyAccount" Component={MyAccount} />
           <Route path="/EditProfile" Component={EditProfile} />
         </Routes>
