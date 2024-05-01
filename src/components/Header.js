7ef450cb56370d89d6e288c716bb8f19abe69114
MUNEERA A <muneeraayakkoden93@gmail.com>
2024-05-01 19:50:42 +0530

    bootstrap components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,29 @@
 import React, { useState } from 'react';
 import logo from '../assets/logo.png';
-//import { useNavigate } from 'react-router-dom';
-import './Header.css';
+//import './Header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
-  {/*const navigate = useNavigate();*/}
   const [showDescription, setShowDescription] = useState(false);
-
-  {/*const handleProfileClick = () => {
-    navigate('/MyAccount');
-  };*/}
-
   const toggleDescription = () => {
     setShowDescription(prevState => !prevState);
   };
 
   return (
-    <header className="header text-white py-3">
-      <div className="row align-items-center">
-        <div className="col">
-          <div className="logo">
-            <img src={logo} alt="Janagrah Logo" />
-          </div>
+    <header className="header bg-white text-primary py-1">
+      <div className="row">
+        <div className="logo">
+          <img src={logo} alt="Janagrah Logo" />
+        </div>
+        <div className="col-md">
+          <h1 className="hero-title text-primary">Your Voice, Shaping Our Ward</h1>
         </div>
       </div>
-      <div className="col-md">
-        <h1 className="hero-title">Your Voice, Shaping Our Ward</h1>
-      </div>
-      <div className="col-auto">
-        <button className="btn btn-outline-light" onClick={toggleDescription}> {showDescription ? 'View Less' : 'Learn More'} </button>
+      <div>
+        <button className="btn btn-outline-primary" onClick={toggleDescription}> {showDescription ? 'View Less' : 'Learn More'} </button>
       </div>
       {showDescription && (
-      <div className="learn-more-description text-primary py-3">
+      <div className="learn-more-description text-primary">
         <div className="paragraphs">
           <p>Janagrah is a resident engagement platform designed to empower residents and strengthen the foundations of our community. We facilitate open communication and informed decision-making by providing a direct line between residents and local authorities.</p>
           <p>Together, we can build a more engaged and informed community.</p>
