500f9d789beee41b76c3dbe818c7d35bf7edbfe8
MUNEERA A <muneeraayakkoden93@gmail.com>
2024-05-21 18:32:55 +0530

    klwqh;o

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/LoginPage');
+  };
+
+    return (
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                <a className="nav-link" href="#" onClick={() => navigate('/ContactPage')}>
+                    <i className="fas fa-envelope"></i> <span className="nav-text">CONTACT</span>
+                </a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#" onClick={() => navigate('/dosurvey')}>
+                    <i className="fas fa-poll"></i> <span className="nav-text">SURVEYS</span>
+                </a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#" onClick={() => navigate('/MyAccount')}>
+                    <i className="fas fa-user"></i> <span className="nav-text">PROFILE</span>
+                </a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#" onClick={handleLogout}>
+                    <i className="fas fa-sign-out-alt"></i> <span className="nav-text">LOGOUT</span>
+                </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
+
--- src/pages/ResidentHome.js
+++ src/pages/ResidentHome.js
@@ -1,50 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
 import Services from './Services';
-import './ResidentHome.css';
-import { useNavigate } from 'react-router-dom';
+//import './ResidentHome.css';
 import Announcement from './Announcement';
 import Footer from '../components/Footer';
 import logo from '../assets/logo.png'; 
-
+import Navbar from '../components/Navbar';
 const ResidentHome = () => {
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/LoginPage');
-  };
-
+  
   return (
     <div className="resident-home">
       <Header>
-        <img src={logo} alt="Janagrah Logo" className="logo" /> {/* Add the logo */}
+        <img src={logo} alt="Janagrah Logo" className="logo" />
         <h1>JANAGRAH</h1>
         <p>Your Voice, Shaping our Ward</p>
       </Header>
-      <nav className="navbar">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#" onClick={() => navigate('/ContactPage')}>
-              <i className="fas fa-envelope"></i> <span className="nav-text">CONTACT</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#" onClick={() => navigate('/dosurvey')}>
-              <i className="fas fa-poll"></i> <span className="nav-text">SURVEYS</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#" onClick={() => navigate('/MyAccount')}>
-              <i className="fas fa-user"></i> <span className="nav-text">PROFILE</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#" onClick={handleLogout}>
-              <i className="fas fa-sign-out-alt"></i> <span className="nav-text">LOGOUT</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+
+      <Navbar />
+
       <main className="main-content">
         <section id="news" className="news-section">
           <Announcement />
